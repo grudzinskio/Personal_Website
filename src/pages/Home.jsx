d03cb09a9f2698e62dd0fce98f765880c3dd60e3
grudzinskio <grudzinskio@msoe.edu>
2025-08-05 14:54:56 -0500

    added hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/Navbar";
-
+import { HeroSection } from "../components/HeroSection";
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         {/* Theme toggle */}
@@ -11,7 +11,9 @@ export const Home = () => {
         {/* Navbar */}
         <Navbar />
         {/* Main contents */}
-
+        <main>
+            <HeroSection />
+        </main>
         {/* Footer */}
 
 
