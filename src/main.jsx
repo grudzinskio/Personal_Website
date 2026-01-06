b8fc01cdd8847248642ad0058d29c60026d9e96f
Oliver Grudzinski <grudzinskioliver@gmail.com>
2026-01-06 11:11:14 -0600

    fixed chrome browser

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Detect Chrome browser and add class to root element (exclude Edge which also contains "Chrome")
+const isChrome = /Chrome/.test(navigator.userAgent) && 
+                 /Google Inc/.test(navigator.vendor) && 
+                 !/Edg/.test(navigator.userAgent);
+if (isChrome) {
+  document.documentElement.classList.add('chrome');
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
